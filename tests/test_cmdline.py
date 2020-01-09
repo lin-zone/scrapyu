eaecc0ac733a23adb2a13123b12158aeb9a3f80b
lin-zone <z_one10@163.com>
2020-01-09 10:56:52 +0800

    fix test cmdline arguments

--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -1,3 +1,4 @@
+import sys
 import subprocess
 from tempfile import mkdtemp, TemporaryFile
 
@@ -6,18 +7,21 @@ from path import Path
 from tests import TEST_DIR
 
 
+args = (sys.executable, '-m', 'scrapyu.cmdline')
+
+
 def test_genspider_list():
-    args = "scrapyu genspider -l"
-    res = subprocess.check_output(args)
+    new_args = args + ('genspider', '-l')
+    res = subprocess.check_output(new_args)
     assert res == b'Available templates:\r\n  single\r\n  single_splash\r\n'
 
 
 def test_single_template():
     single_test_template = Path(TEST_DIR) / 'test_templates' / 'single.py'
     cwd = mkdtemp()
-    args = "scrapyu genspider single www.scrapytest.org -t single"
+    new_args = args + ('genspider', 'single', 'www.scrapytest.org', '-t', 'single')
     with TemporaryFile() as out:
-        subprocess.call(args, stdout=out, stderr=out, cwd=cwd)
+        subprocess.call(new_args, stdout=out, stderr=out, cwd=cwd)
     t = Path(cwd) / 'single.py'
     assert t.exists() is True
     assert t.read_text() == single_test_template.read_text()
@@ -26,9 +30,9 @@ def test_single_template():
 def test_single_splash_template():
     single_splash_test_template = Path(TEST_DIR) / 'test_templates' / 'single_splash.py'
     cwd = mkdtemp()
-    args = "scrapyu genspider single-splash www.scrapytest.org -t single_splash"
+    new_args = args + ('genspider', 'single-splash', 'www.scrapytest.org', '-t', 'single_splash')
     with TemporaryFile() as out:
-        subprocess.call(args, stdout=out, stderr=out, cwd=cwd)
+        subprocess.call(new_args, stdout=out, stderr=out, cwd=cwd)
     t = Path(cwd) / 'single_splash.py'
     assert t.exists() is True
     assert t.read_text() == single_splash_test_template.read_text()
